705524d90238b7eb1b7ac4aace97a2115302c631
omar <omar@e3d1ee71-de1f-0410-997d-c91044e7a769>
2013-07-23 19:51:08 +0000

    shortened documentation + 2013 date

--- meka/srcs/shared.h
+++ meka/srcs/shared.h
@@ -21,7 +21,7 @@
 #define MEKA_HOMEPAGE           "http://www.smspower.org/meka"
 #define MEKA_AUTHORS            "Omar Cornut (Bock) and contributors"
 #define MEKA_AUTHORS_SHORT      "Omar Cornut and contributors"
-#define MEKA_DATE               "1998-2012"
+#define MEKA_DATE               "1998-2013"
 
 extern char MEKA_BUILD_DATE[];
 extern char MEKA_BUILD_TIME[];
