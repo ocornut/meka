b0bb9343b92dabee5b232ace0856a9bb142a8a69
omar <omar@e3d1ee71-de1f-0410-997d-c91044e7a769>
2013-07-23 11:57:24 +0000

    Added a batch script 'tools/dist_bin_win32.bat' to create binary Win32 package.

---
+++ trunk/meka/tools/dist_bin_win32.bat
@@ -0,0 +1,15 @@
+@echo Creating Dist/mekaw.zip
+cd ..
+mkdir Dist
+@echo Compressing
+tools\dist_win\upx.exe -9 mekaw.exe
+@echo Packaging
+tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.blt meka.dat mekaw.cfg mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
+tools\dist_win\zip.exe -9 Dist\mekaw.zip icons.zip setup.bat
+tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.txt changes.txt compat.txt debugger.txt multi.txt tech.txt
+tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Themes\*.png Themes\Credits.txt
+tools\dist_win\zip.exe -9 Dist\mekaw.zip -r datafiles\*.*
+@echo Done!
+@echo -- Check MEKA.BLT ! --
+@echo -- Check MEKA.INP ! Joypad auto, on --
+pause
