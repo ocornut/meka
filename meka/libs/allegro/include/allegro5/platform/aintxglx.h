fb25fb0f91bd67b01febc006c27ed19e67ada1bf
omar <omar@e3d1ee71-de1f-0410-997d-c91044e7a769>
2013-07-24 00:06:58 +0000

    Allegro 5.0.9 msvc binaries

---
+++ meka/libs/allegro/include/allegro5/platform/aintxglx.h
@@ -0,0 +1,8 @@
+#ifndef __al_included_allegro5_aintxglx_h
+#define __al_included_allegro5_aintxglx_h
+
+ALLEGRO_DISPLAY_INTERFACE *_al_display_xglx_driver(void);
+ALLEGRO_SYSTEM_INTERFACE *_al_system_xglx_driver(void);
+ALLEGRO_BITMAP_INTERFACE *_al_bitmap_xglx_driver(void);
+
+#endif
