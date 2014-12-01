04ea33ccc6f310e2eb32b30181809ce7414fd913
omar <omar@e3d1ee71-de1f-0410-997d-c91044e7a769>
2014-12-01 16:35:10 +0000

    WIP loading TTF fonts - dist script

--- trunk/meka/tools/dist_bin_win32.bat
+++ trunk/meka/tools/dist_bin_win32.bat
@@ -9,6 +9,7 @@ tools\dist_win\zip.exe -9 Dist\mekaw.zip icons.zip setup.bat
 tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.txt changes.txt compat.txt debugger.txt multi.txt tech.txt
 tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Themes\*.png Themes\Credits.txt
 tools\dist_win\zip.exe -9 Dist\mekaw.zip -r datafiles\*.*
+tools\dist_win\zip.exe -9 Dist\mekaw.zip -r datafiles\fonts\*.*
 @echo Done!
 @echo -- Check MEKA.BLT ! --
 @echo -- Check MEKA.INP ! Joypad auto, on --
