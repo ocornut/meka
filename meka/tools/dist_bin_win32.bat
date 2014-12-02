d2f23e3247c62327b3b304e799ad61360278c946
omar <omar@e3d1ee71-de1f-0410-997d-c91044e7a769>
2014-12-02 12:41:49 +0000

    Moved datafiles/ to Data/ folder (uppercase = user-facing folders)

--- meka/tools/dist_bin_win32.bat
+++ meka/tools/dist_bin_win32.bat
@@ -7,9 +7,9 @@ tools\dist_win\upx.exe -9 mekaw.exe
 tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.blt meka.dat mekaw.cfg mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
 tools\dist_win\zip.exe -9 Dist\mekaw.zip icons.zip setup.bat
 tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.txt changes.txt compat.txt debugger.txt multi.txt tech.txt
+tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Data\*.*
+tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Data\fonts\*.*
 tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Themes\*.png Themes\Credits.txt
-tools\dist_win\zip.exe -9 Dist\mekaw.zip -r datafiles\*.*
-tools\dist_win\zip.exe -9 Dist\mekaw.zip -r datafiles\fonts\*.*
 @echo Done!
 @echo -- Check MEKA.BLT ! --
 @echo -- Check MEKA.INP ! Joypad auto, on --
