74c05d64b21896eb7393a614fad41969d5d9527b
ocornut <omarcornut@gmail.com>
2016-03-22 11:16:40 +0100

    Remove old libs

--- meka/libs/allegro/include/allegro5/internal/aintern_transform.h
+++
@@ -1,9 +0,0 @@
-#ifndef __al_included_allegro5_aintern_transform_h
-#define __al_included_allegro5_aintern_transform_h
-
-
-bool _al_transform_is_translation(const ALLEGRO_TRANSFORM* trans,
-   float *dx, float *dy);
-
-
-#endif
