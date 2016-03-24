b66c7e69c27834296378a9f0c2037f1443e78ca0
ocornut <omarcornut@gmail.com>
2016-03-24 23:11:48 +0100

    Tools: Dist: Not bundling mekaw.cfg in Windows binaries packages (#5)

--- meka/tools/dist_bin_win32.bat
+++ meka/tools/dist_bin_win32.bat
@@ -4,7 +4,8 @@ mkdir Dist
 @echo Compressing
 tools\dist_win\upx.exe -9 mekaw.exe
 @echo Packaging
-tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.blt meka.dat mekaw.cfg mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
+tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.blt meka.dat mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
+@REM tools\dist_win\zip.exe -9 Dist\mekaw.zip mekaw.cfg
 tools\dist_win\zip.exe -9 Dist\mekaw.zip icons.zip setup.bat
 tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.txt changes.txt compat.txt debugger.txt multi.txt tech.txt
 tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Data\*.*
