22f8f525ef4e4515457ddc9a201a2dc156731944
Maxim <maxim@smspower.org>
2019-02-05 09:33:19 +0000

    Build: Fixes on dist_bin_win32.bat (#39)
    Pass -X flag to zip to save a few bytes (omitting NTFS security metadata)
    Removing redundant zip invocation
    Correcting zip command which was referring to a renamed file

--- meka/tools/dist_bin_win32.bat
+++ meka/tools/dist_bin_win32.bat
@@ -4,13 +4,12 @@ mkdir Dist
 @echo Compressing
 tools\dist_win\upx.exe -9 mekaw.exe
 @echo Packaging
-tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.blt meka.dat mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
-@REM tools\dist_win\zip.exe -9 Dist\mekaw.zip mekaw.cfg
-tools\dist_win\zip.exe -9 Dist\mekaw.zip icons.zip setup.bat
-tools\dist_win\zip.exe -9 Dist\mekaw.zip meka.txt changes.txt compat.txt debugger.txt multi.txt tech.txt
-tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Data\*.*
-tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Data\fonts\*.*
-tools\dist_win\zip.exe -9 Dist\mekaw.zip -r Themes\*.png Themes\Credits.txt
+tools\dist_win\zip.exe -X9 Dist\mekaw.zip meka.blt meka.dat mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
+@REM tools\dist_win\zip.exe -X9 Dist\mekaw.zip mekaw.cfg
+tools\dist_win\zip.exe -X9 Dist\mekaw.zip icons.zip setup.bat
+tools\dist_win\zip.exe -X9 Dist\mekaw.zip meka.txt changes.txt compat.txt debugger.txt multi.txt tech.txt
+tools\dist_win\zip.exe -X9 Dist\mekaw.zip -r Data\*.*
+tools\dist_win\zip.exe -X9 Dist\mekaw.zip -r Themes\*.png Themes\README.txt
 @echo Done!
 @echo -- Check MEKA.BLT ! --
 @echo -- Check MEKA.INP ! Joypad auto, on --
