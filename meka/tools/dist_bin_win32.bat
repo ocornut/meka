c00bce7c638f2f6d094f5e20ee038b944e913bf9
omar <ocornut@users.noreply.github.com>
2021-07-26 14:25:03 +0200

    Build: stop using UPX

--- meka/tools/dist_bin_win32.bat
+++ meka/tools/dist_bin_win32.bat
@@ -1,8 +1,10 @@
 @echo Creating Dist/mekaw.zip
 cd ..
 mkdir Dist
-@echo Compressing
-tools\dist_win\upx.exe -9 mekaw.exe
+
+@REM @echo Compressing
+@REM tools\dist_win\upx.exe -9 mekaw.exe
+
 @echo Packaging
 tools\dist_win\zip.exe -X9 Dist\mekaw.zip meka.blt meka.dat mekaw.exe meka.inp meka.msg meka.nam meka.pat meka.thm
 @REM tools\dist_win\zip.exe -X9 Dist\mekaw.zip mekaw.cfg
